2a3d37703ba32e3f76e24652ea2efdc9314fa5eb
Alex <arkohanim@pipeline.sbcc.edu>
2019-04-12 15:16:23 -0700

    more brainstorming

--- mapMaze.py
+++ mapMaze.py
@@ -2,6 +2,9 @@
 
 """
     Positions for bot: "North" "South" "East" "West"
+    4 startting potiential goals, center + North, center + East...
+    3 x 3 sub cells, to move from positions, move 2 cells ignore corners
+
 """
 
 import random
@@ -10,12 +13,14 @@ import random
 class mapMaze:
     """ Micro mouse Maze solver? """
 
-    def __init__(self, x_init=0, y_init=0, width=11, height=11):
+    def __init__(self, x_init=0, y_init=0, width=31, height=31):
         self.x = x_init
         self.y = y_init
         self.width = width
         self.height = height
-        self.matrix = [["open" for _ in range(height)] for __ in range(width)]
+        self.matrix = [[0 for _ in range(height)] for __ in range(width)]
+        floodfill(0, width//2, height //2)
+        
 
     def invokeLeftMotor(self, count):
         # invoke motor for turn, multiplier is count
@@ -58,41 +63,42 @@ class mapMaze:
         return True if random.randrange(0, 2) == 0 else False
         pass
 
-    def floodfill(self, matrix, x, y):
+    def floodfill(self, distance, matrix, x, y, dx, dy):
         """ Source: https://stackoverflow.com/a/19840816 
-            matrix contains values "open" "wall" and "connected"
-            initial matrix is filled with "open"
+            matrix contains integer values
+            initial matrix is filled with goal posts and 1's
         """
         # if at a wall, don't invoke
-        if matrix[x][y] == "open":
-            matrix[x][y] = "connected"
+        if matrix[x][y] >= 0:
+            matrix[x][y] = distance
             # recursively invoke flood fill on all surrounding cells:
             if x > 0:
-                floodfill(matrix, x-1, y)
+                floodfill(matrix, x + dx, y)
             if x < len(matrix[y]) - 1:
-                floodfill(matrix, x+1, y)
+                floodfill(matrix, x - dx, y)
             if y > 0:
-                floodfill(matrix, x, y-1)
+                floodfill(matrix, x, y- dy)
             if y < len(matrix) - 1:
-                floodfill(matrix, x, y+1)
+                floodfill(matrix, x, y+ dy)
         return matrix
 
-    def checkForWall(self):
+    def checkForWall(self, x=self.x, y=self.y):
+        offSets = {"North" : (1, 1), "South": (-1, -1), "West": (1, -1), "East": (-1, 1)} 
         if self.x < width:
             if invokeRightSensor():
                 self.matrix[x+1][y] = "wall"
-                self.matrix = floodfill(self.matrix, self.x + 1, self.y)
+                self.matrix = floodfill(self.matrix, x + 1, x)
             else:
                 self.matrix[x+1][y] = "open"
         if self.x > 0:
             if invokeLeftSensor():
                 self.matrix[x-1][y] = "wall"
-                self.matrix = floodfill(self.matrix, self.x - 1, self.y)
+                self.matrix = floodfill(self.matrix, x - 1, y)
             else:
                 self.matrix[x-1][y] = "open"
         if self.y < height:
             if invokeForwardSensor():
                 self.matrix[x][y+1] = "wall"
-                self.matrix = floodfill(self.matrix, self.x, self.y + 1)
+                self.matrix = floodfill(self.matrix, x, y + 1)
             else:
                 self.matrix[x][y+1] = "open"
